0a7f88066d7f5dd9bf433f0e3a766a9067e08924
sgomezmelo <37335429+sgomezmelo@users.noreply.github.com>
2018-04-04 20:31:03 +0000

    Add files via upload

---
+++ SantiagoGomez_Graficas.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos_python = np.loadtxt('times_python.csv', delimiter = ' ')
+datos_cpp = np.loadtxt('times_cpp.csv', delimiter = ' ')
+
+plt.figure()
+plt.plot(datos_cpp[:,0],datos_cpp[:,1], label = 'c++')
+plt.plot(datos_python[:,0],datos_python[:,1], label = 'python')
+plt.xlabel('N')
+plt.ylabel('Tiempo en s')
+plt.legend()
+plt.savefig('cpp_vs_python.png')
